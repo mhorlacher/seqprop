4e5a803be6062e248dba81d6853787f4b2152856
marc.horlacher <marc.horlacher@bayer.com>
2024-10-27 17:43:13 +0100

    added motif penalty

--- seqprop/penalties.py
+++ seqprop/penalties.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from seqprop.templating import sequence_to_multihot
 
 class DoubleGpCPenalty(nn.Module):
-    def __init__(self):
+    def __init__(self, motif_multihot: torch.Tensor):
         super().__init__()
         self.conv1d = nn.Conv1d(4, 1, 4, bias=True)
         self.conv1d.weight = nn.Parameter(sequence_to_multihot('GCGC').float().unsqueeze(0).transpose(1, 2))
@@ -16,3 +16,20 @@ class DoubleGpCPenalty(nn.Module):
 
     def forward(self, inputs):
         return F.relu(self.conv1d(inputs.transpose(1, 2))).sum(dim=-1).mean()
+
+class MotifPenalty(nn.Module):
+    def __init__(self, motif_multihot: torch.Tensor, exclude_positions: list[int] | None = None):
+        super().__init__()
+        assert len(motif_multihot.shape) == 2
+        self.exclude_positions = exclude_positions
+
+        self.conv1d = nn.Conv1d(motif_multihot.shape[1], 1, motif_multihot.shape[0], bias=True)
+        self.conv1d.weight = nn.Parameter(motif_multihot.float().unsqueeze(0).transpose(1, 2))
+        self.conv1d.bias = nn.Parameter(torch.tensor([torch.tensor(-motif_multihot.shape[0] + 1, dtype=torch.float32)]))
+
+    def forward(self, inputs):
+        return F.relu(self.conv1d(inputs.transpose(1, 2))).squeeze(1).sum(dim=-1)
+    
+    @classmethod
+    def from_sequence(cls, motif_sequence: str):
+        return cls(sequence_to_multihot(motif_sequence))
